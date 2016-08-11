537970f51014b7199417efcd910502033eb4267e
User106 <mac@FruitdeMacBook-Pro.local>
2016-08-11 21:09:24 +0800

    init

---
+++ JS2OCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JS2OCDemo
+//
+//  Created by Fruit on 16/7/26.
+//  Copyright © 2016年 chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
